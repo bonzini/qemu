c5448b5677a470c93259d0c604dbd87cca7d0831
Paolo Bonzini <pbonzini@redhat.com>
2020-10-21 10:27:09 -0400

    vl: move semihosting command line fallback to qemu_finish_machine_init
    Move more sane parts of the huge qemu_init function out of it.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -3070,6 +3070,11 @@ static void qemu_init_board(void)
 {
     MachineClass *machine_class = MACHINE_GET_CLASS(current_machine);
 
+    if (semihosting_enabled() && !semihosting_get_argc() && current_machine->kernel_filename) {
+        /* fall back to the -kernel/-append */
+        semihosting_arg_fallback(current_machine->kernel_filename, current_machine->kernel_cmdline);
+    }
+
     if (machine_class->default_ram_id && current_machine->ram_size &&
         numa_uses_legacy_mem() && !current_machine->ram_memdev_id) {
         create_default_memdev(current_machine, mem_path);
@@ -4387,13 +4392,6 @@ void qemu_init(int argc, char **argv, char **envp)
 
     if (!boot_order) {
         boot_order = machine_class->default_boot_order;
-    }
-
-    if (semihosting_enabled() && !semihosting_get_argc()) {
-        const char *kernel_filename = qemu_opt_get(machine_opts, "kernel");
-        const char *kernel_cmdline = qemu_opt_get(machine_opts, "append");
-        /* fall back to the -kernel/-append */
-        semihosting_arg_fallback(kernel_filename, kernel_cmdline);
     }
 
     if (net_init_clients(&err) < 0) {
